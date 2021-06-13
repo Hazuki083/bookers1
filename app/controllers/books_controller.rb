dc89cfbf047f543a46a39aadedb74874f2f1878e
Hazuki083 <ha882ki@gmail.com>
2021-06-13 12:00:04 +0000

    初めてのプッシュ

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,39 @@
+class BooksController < ApplicationController
+
+  def index
+  end
+
+  def show
+
+  end
+
+  def new
+
+
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+
+
+  end
+
+  def edit
+
+  end
+  def update
+
+
+  end
+  def destroy
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
