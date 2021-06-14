47934b96cebef2ae3e76b99daeb49700123c1ae7
Hazuki083 <ha882ki@gmail.com>
2021-06-14 03:28:04 +0000

    詳細画面に編集画面のリンク実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
 
   end
 
@@ -22,6 +23,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
 
   end
   def update
